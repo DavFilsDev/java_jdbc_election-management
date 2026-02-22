4738ea777a270bec8d945ecaf2d6783f105eccb7
DavFilsDev <miharisoadavidfils@gmail.com>
2026-02-22 13:22:46 +0300

    test: add main output for election turnout rate calculation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,23 +9,28 @@ public class Main {
 
         DataRetriever dataRetriever = new DataRetriever();
 
-        // Q1 Test
+        // Q1
         long totalVote = dataRetriever.countAllVotes();
         System.out.println("totalVote=" + totalVote);
 
-        // Q2 Test
-        System.out.println("\n=== Q2 - Votes by Type ===");
+        // Q2
+        System.out.println("\n Q2: Votes by Type");
         List<VoteTypeCount> voteTypeCounts = dataRetriever.countVotesByType();
         System.out.println(voteTypeCounts);
 
         // Q3
-        System.out.println("\n=== Q3 Valid Votes by Candidate ===");
+        System.out.println("\n Q3: Valid Votes by Candidate");
         List<CandidateVoteCount> validVotes = dataRetriever.countValidVotesByCandidate();
         System.out.println(validVotes);
 
         // Q4
-        System.out.println("\n=== Q4 Global Vote Summary ===");
+        System.out.println("\n Q4: Global Vote Summary");
         VoteSummary summary = dataRetriever.computeVoteSummary();
         System.out.println(summary);
+
+        // Q5
+        System.out.println("\n Q5: Turnout Rate");
+        double turnout = dataRetriever.computeTurnoutRate();
+        System.out.println("Turnout Rate = " + turnout + "%");
     }
 }
