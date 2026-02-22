d5f78fab824f1b94cf77f9f1491f46b6cb52a31a
DavFilsDev <miharisoadavidfils@gmail.com>
2026-02-22 12:49:57 +0300

    feat: display total vote count in main application

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import services.DataRetriever;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DataRetriever dataRetriever = new DataRetriever();
+
+        long totalVote = dataRetriever.countAllVotes();
+        System.out.println("totalVote=" + totalVote);
+    }
+}
