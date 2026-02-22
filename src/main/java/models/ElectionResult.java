1af3902739b19bb681da4810382bb27a70306dea
DavFilsDev <miharisoadavidfils@gmail.com>
2026-02-22 13:25:12 +0300

    feat: add ElectionResult DTO to represent election winner aggregation

---
+++ src/main/java/models/ElectionResult.java
@@ -0,0 +1,25 @@
+package models;
+
+public class ElectionResult {
+
+    private String candidateName;
+    private long validVoteCount;
+
+    public ElectionResult(String candidateName, long validVoteCount) {
+        this.candidateName = candidateName;
+        this.validVoteCount = validVoteCount;
+    }
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public long getValidVoteCount() {
+        return validVoteCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Winner: " + candidateName + " with " + validVoteCount + " valid votes";
+    }
+}
