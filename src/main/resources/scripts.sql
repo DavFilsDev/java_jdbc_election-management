800ac67b0e4297796ec4b5ffe8108d2daf24fce4
DavFilsDev <miharisoadavidfils@gmail.com>
2026-02-22 11:24:00 +0300

    feat(init data): add script to insert initial data into the vote table

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -45,3 +45,10 @@ INSERT INTO voter (name) VALUES
                                 ('Voter4'),
                                 ('Voter5'),
                                 ('Voter6');
+INSERT INTO vote (candidate_id, voter_id, vote_type) VALUES
+                                                         (1, 1, 'VALID'),
+                                                         (1, 2, 'VALID'),
+                                                         (2, 3, 'VALID'),
+                                                         (2, 4, 'BLANK'),
+                                                         (NULL, 5, 'BLANK'),
+                                                         (3, 6, 'NULL');
