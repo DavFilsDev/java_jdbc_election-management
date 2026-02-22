659a910623a0a804998f5a2a4302bbfa1ec1783b
DavFilsDev <miharisoadavidfils@gmail.com>
2026-02-22 13:02:37 +0300

    test: add main test for vote count by type aggregation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import models.*;
 import services.DataRetriever;
+
+import java.util.List;
 
 public class Main {
 
@@ -6,7 +9,13 @@ public class Main {
 
         DataRetriever dataRetriever = new DataRetriever();
 
+        // Q1 Test
         long totalVote = dataRetriever.countAllVotes();
         System.out.println("totalVote=" + totalVote);
+
+        // Q2 Test
+        System.out.println("\n=== Q2 - Votes by Type ===");
+        List<VoteTypeCount> voteTypeCounts = dataRetriever.countVotesByType();
+        System.out.println(voteTypeCounts);
     }
 }
