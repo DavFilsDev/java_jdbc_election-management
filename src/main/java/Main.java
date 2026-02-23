8e0875c866c82d8878fc05b80337f6c26f193c83
DavFilsDev <miharisoadavidfils@gmail.com>
2026-02-23 20:40:50 +0300

    refactor: update the display of the result and format the code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,32 +9,26 @@ public class Main {
 
         DataRetriever dataRetriever = new DataRetriever();
 
-        // Q1
         long totalVote = dataRetriever.countAllVotes();
         System.out.println("totalVote=" + totalVote);
 
-        // Q2
         System.out.println("\n Q2: Votes by Type");
         List<VoteTypeCount> voteTypeCounts = dataRetriever.countVotesByType();
         System.out.println(voteTypeCounts);
 
-        // Q3
         System.out.println("\n Q3: Valid Votes by Candidate");
         List<CandidateVoteCount> validVotes = dataRetriever.countValidVotesByCandidate();
         System.out.println(validVotes);
 
-        // Q4
         System.out.println("\n Q4: Global Vote Summary");
         VoteSummary summary = dataRetriever.computeVoteSummary();
         System.out.println(summary);
 
-        // Q5
         System.out.println("\n Q5: Turnout Rate");
         double turnout = dataRetriever.computeTurnoutRate();
         System.out.println("Turnout Rate = " + turnout + "%");
 
-        // Q6
-        System.out.println("\n=== Q6 Election Winner ===");
+        System.out.println("\n Q6 Election Winner");
         ElectionResult winner = dataRetriever.findWinner();
         System.out.println(winner);
     }
